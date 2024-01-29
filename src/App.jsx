7fccc77b5d70fb8925f199475c2d6b3ba1d8d140
Muhammad Tamim <mdt40234@gmail.com>
2024-01-30 01:04:21 +0600

    create responsive navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Nav/header'
+{
+  /* The following line can be included in your src/index.js or App.js file */
+}
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
 
   return (
     <>
-
+      <Header></Header>
     </>
   )
 }
